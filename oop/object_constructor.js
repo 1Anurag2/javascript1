1970d59b898f6c3621dd169b4b2e1f1d3ce530bf
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2024-08-26 21:53:39 +0530

    .

--- oop/object_constructor.js
+++ oop/object_constructor.js
@@ -5,3 +5,5 @@ function person(name,age){
 let person1 = new person("Alice",21)
 console.log(person1)
 console.log(`My name is ${person.name} verma and I am ${person.age} years old`)
+console.log(typeof person)
+console.log(typeof person1)
