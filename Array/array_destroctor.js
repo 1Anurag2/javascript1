21a00cb3fd5e6449e7affb896bc8aaa916879772
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2024-08-26 22:12:24 +0530

    .

---
+++ Array/array_destroctor.js
@@ -0,0 +1,21 @@
+// Array Destructuring
+
+let [first,second] = [54,"string"]
+console.log(first)
+console.log(second)
+
+// Multidimensional Arrays
+
+let matrix = [
+    [4,6,9],
+    [7,9,5],
+    [8,0,2]
+];
+let element = matrix[0][1]; 
+console.log(element); 
+
+let element2 = matrix[2][2];
+console.log(element2)
+
+let element3 = matrix[1][2]
+console.log(element3)
