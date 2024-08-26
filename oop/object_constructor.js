70d110f8cb6284ac0bf55ca65d28595b6f749584
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2024-08-26 21:52:24 +0530

    .

---
+++ oop/object_constructor.js
@@ -0,0 +1,7 @@
+function person(name,age){
+    this.name = name
+    this.age = age
+}
+let person1 = new person("Alice",21)
+console.log(person1)
+console.log(`My name is ${person.name} verma and I am ${person.age} years old`)
