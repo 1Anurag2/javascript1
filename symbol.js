acdd27d08e8aa5d58ad83b8f9aaddaec97f3c7d1
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-04-05 01:01:59 +0530

    symbol

---
+++ symbol.js
@@ -0,0 +1,8 @@
+let x  = Symbol("Hello")     // Symbol define a unique values 
+let y  = Symbol("Hello")
+console.log(typeof x)
+console.log(x == y)
+
+let str1  = "Hello"
+let str2  = "Hello"
+console.log(str1 == str2)
