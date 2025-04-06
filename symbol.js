3507bb0ca924294c12e505ac6309b86a39eb7e96
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-04-07 00:42:55 +0530

    symbol

--- symbol.js
+++ symbol.js
@@ -1,8 +1,19 @@
 let x  = Symbol("Hello")     // Symbol define a unique values 
-let y  = Symbol("Hello")
+let y  = Symbol("Hello")     // symbol not use in for loop
 console.log(typeof x)
 console.log(x == y)
+console.log(x)
+// alert(x.toString());
+// alert(x.description);
 
 let str1  = "Hello"
 let str2  = "Hello"
 console.log(str1 == str2)
+
+let age = Symbol();
+let user = {
+    name : "Anurag",
+    class : 'Btech',
+    [age] : 22
+}
+console.log(user)  
