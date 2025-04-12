855a50408439b09a57e28bf2fd10290ee77638bc
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-04-13 00:08:07 +0530

    loops

---
+++ loops/for_each.js
@@ -0,0 +1,13 @@
+const array = [1,2,3,4,5,6];
+
+array.forEach(element => {
+    console.log(element)
+})
+// const newArray = array.forEach(element => {
+//     console.log(element)
+// });
+
+// forEach() is used only for iteration — it does not return a new array.
+// So newArray will be undefined because forEach() always returns undefined.
+
+// console.log(newArray)
--- loops/map.js
+++ loops/map.js
@@ -1,5 +1,6 @@
 const array = [2,3,4,5,6,7,8];
 
+// map return a new array it's not change the original array value 
 const newarray = array.map((value) => {
     value += 2
     console.log(value)
