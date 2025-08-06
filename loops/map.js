65e9e5278db32388fa2f5b3c3bb52c8df2246cbd
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-08-06 16:31:39 +0530

    map loop

--- loops/map.js
+++ loops/map.js
@@ -1,10 +1,10 @@
-const array = [2,3,4,5,6,7,8];
+const array = [2, 3, 4, 5, 6, 7, 8];
 
-// map return a new array it's not change the original array value 
+// map return a new array it's not change the original array value
 const newarray = array.map((value) => {
-    value += 2
-    console.log(value)
-})
+  value += 2;
+  console.log(value);
+});
 
-console.log(typeof array)
-console.log(typeof newarray)
+console.log(typeof array);
+console.log(typeof newarray);
