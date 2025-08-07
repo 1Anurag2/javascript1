967ac2747914ea51e5fc36d9f1a27e9794cf893f
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-08-07 16:50:05 +0530

    object_destructuring

---
+++ ES6/object_destructure.js
@@ -0,0 +1,16 @@
+let obj = {
+    name : "Anurag",
+    age : 22,
+    city : "Mrj"
+}
+let {name : n, city : c} = obj; // Object destructuring
+// console.log(name)
+// // console.log(age)
+// console.log(city)
+
+console.log(n)
+console.log(c)
+
+
+
+
